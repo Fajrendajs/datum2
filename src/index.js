27f3741d9684f3f2f94b141e81e70b4789b41109
Fajrendajs <40194093+Fajrendajs@users.noreply.github.com>
2019-04-14 13:36:30 +0200

    checkbox

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import moment from 'moment';
-import 'moment/locale/ru';
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import moment from "moment";
+import "moment/locale/ru";
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import './index.css';
-import Tab from './Tab';
-import Atlas from './Atlas';
-import Dat from './Dat';
-import But from './But';
-import Table from './Table';
+import "./index.css";
+import Tab from "./Tab";
+import Atlas from "./Atlas";
+import Dat from "./Dat";
+import But from "./But";
+import Table from "./Table";
+import Checkbox from "./Checkbox";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    moment.locale('ru');
+    moment.locale("ru");
   }
 
   render() {
@@ -24,9 +25,10 @@ class App extends Component {
         <But />
         <Dat />
         <Table />
+        <Checkbox />
       </>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
